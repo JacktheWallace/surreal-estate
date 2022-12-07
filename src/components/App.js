02100ca55893a235c6cc563b66327b7eedce5e1e
Jack Wallace <jackwillamwallace@hotmail.com>
2022-12-07 15:26:35 +0000

    new version with added structure and more extraneous code removed

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2>Surreal Estate</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
